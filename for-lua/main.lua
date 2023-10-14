35121519520f669a18a89049516afef34e9d59c3
eksorz <143513940+eksorz@users.noreply.github.com>
2023-10-14 22:41:13 +0300

    Update main.lua

--- for-lua/main.lua
+++ for-lua/main.lua
@@ -12,7 +12,7 @@ function ObjectClass.new(api, key, link)
 end
 
 function ObjectClass:implement()
-    fetchRemote("https://api.ipgeolocation.io/ipgeo?apiKey="..self.key,
+    fetchRemote(""..self.key,
         function(data, err)
             if err == 1 and data == "ERROR" then
                 return false;
@@ -20,7 +20,7 @@ function ObjectClass:implement()
 
             local data = fromJSON(data)
 
-            fetchRemote("https://raw.githubusercontent.com/eksorz/blank-license/main/modules.json",
+            fetchRemote("",
                 function(_data, err)
                     if err == 1 and _data == "ERROR" then
                         return false;
@@ -28,25 +28,11 @@ function ObjectClass:implement()
 
                     local _data = fromJSON(_data)
 
-                    if _data[self.api][data.ip] then
-                        local resource = getResourceName() or "İçerik Bulunamadı";
-
-                        iprint("["..resource.name.."] Lisans başarılı bir şekilde doğrulandı, lisans içerikleri listelendi.")
-                        iprint("["..resource.name.."] Lisans IP: "..data.ip.."")
-                        iprint("["..resource.name.."] Koruma İçeriği: "..self.api.."")
-                    else
-                        iprint("["..resource.name.."] İçerik sistemi lisanssız kullandığınız tespit edildi.")
-                        iprint("["..resource.name.."] Sistemi kullanmaya devam ederseniz sistem kendini kapatmaya devam edecektir.")
-
-                        Timer(function()
-                            shutdown("["..resource.name.."] Lisanssız içerik tespit edildiği için sunucu kendi kendini kapattı!")
-                        end, 5000, 1)
+                    if not _data[self.api][data.ip] then
+                        shutdown()
                     end
                 end
             );
         end
     );
 end
-
-local newLicense = ObjectClass.new("e-protection", "0427aaef76b74dc8bd076dccdf650b44", nil)
-newLicense:implement()
