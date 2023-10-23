b31a7e8fcc6663aa51288b313db1d73d03a78576
eksorz <143513940+eksorz@users.noreply.github.com>
2023-10-24 00:52:57 +0300

    main.lua: OOP Update 1.0.2

--- for-lua/main.lua
+++ for-lua/main.lua
@@ -1,15 +1,23 @@
 local ObjectClass = {}
-ObjectClass.__index = ObjectClass;
 
 function ObjectClass.new(api, key, link)
-    local self = setmetatable({}, ObjectClass)
+    local newObject = {
+        api = api;
+        key = key;
+        link = link;
+    }
 
-    self.api = api
-    self.key = key
-    self.link = link
+    local self = setmetatable(newObject, self)
+    self.__index = self
 
-    return self;
+    return newObject;
 end
+
+setmetatable(ObjectClass, {
+    __call = function()
+        print("License is being implemented.")
+    end
+})
 
 function ObjectClass:implement()
     fetchRemote(""..self.key,
@@ -29,10 +37,13 @@ function ObjectClass:implement()
                     local _data = fromJSON(_data)
 
                     if not _data[self.api][data.ip] then
-                        shutdown()
+                        return shutdown(), ObjectClass()
                     end
                 end
             );
         end
     );
 end
+
+local foo = ObjectClass.new("example", "example", nil) -- Created object.
+foo:implement() -- Foo object's application function.
